3de80bc27c9480d2e815606ebed80a594e246a74
li-next <lidoublenext@gmail.com>
2021-05-05 10:36:19 +0500

    three

--- src/test/java/ru/ffclub/GetTestSite.java
+++ src/test/java/ru/ffclub/GetTestSite.java
@@ -41,7 +41,7 @@ public class GetTestSite extends Base{
         //нет никакого сообщение от том что надо вести login и password страница просто перезагружается
     }
 
-    
+
 
     //НЕ верный ввод login and password
     @Test
